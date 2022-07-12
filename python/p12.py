6a1a3d0b13c1ef24d3d50893c04b296ecff9a423
Ruchikar123 <103450812+Ruchikar123@users.noreply.github.com>
2022-07-12 12:52:26 +0530

    update

--- python/p12.py
+++
@@ -1,6 +0,0 @@
-def HandleLogout(request):
-        del request.session['is_logged']
-        del request.session["user_id"] 
-        logout(request)
-        messages.success(request, " Successfully logged out")
-        return redirect('dashboard')
--- python/p8.py
+++
@@ -1,6 +0,0 @@
-def deletebook(request,id):
-    if request.session.has_key('is_logged'):
-        AddBook_info = AddBook.objects.get(id=id)
-        AddBook_info.delete()
-        return redirect("dashboard")
-    return redirect("login") 
